90233f461d7e19ec1b795310e4c0fa446e9226bc
Sara Oliver <s.oliver1@icloud.com>
2021-04-12 22:34:21 +0000

    new images add

--- services/urls.py
+++ services/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
 
     path('services/about', views.about, name='about'),
     path('services/our_brands', views.our_brands, name='our_brands'),
-    path('services/our_services', views.our_services, name='our_services'),
+    path('services/about_us', views.about_us, name='about_us'),
     path('services/brands', views.brands, name='brands'),
     path('services/contact', views.contact, name='contact'),
     path('services/graphic_design',

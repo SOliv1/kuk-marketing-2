124f983ae923f29584cc08c5efc1fbdf0084ecb0
SOliv1 <s.oliver1@icloud.com>
2021-04-15 01:49:46 +0100

    update settings

--- kuk_marketing/settings.py
+++ kuk_marketing/settings.py
@@ -25,10 +25,11 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ.get("SECRET_KEY", "my secret key here")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = 'DEVELOPMENT' in os.environ
-DEBUG = True
+from os import path
+if path.exists("env.py"):
+  import env
 
-ALLOWED_HOSTS = ['127.0.0.1 localhost' ]
+LLOWED_HOSTS = ['127.0.0.1', "HOST",'localhost']
 
 
 # Application definition

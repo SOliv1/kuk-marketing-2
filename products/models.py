fc9953c31df7cd193f335103445fea44047812bb
SOliv1 <s.oliver1@icloud.com>
2021-04-30 19:19:25 +0100

    add images and amend fixtures

--- products/models.py
+++ products/models.py
@@ -22,7 +22,6 @@ class Product(models.Model):
                                  on_delete=models.SET_NULL)
     name = models.CharField(max_length=254)
     description = models.TextField()
-    has_sizes = models.BooleanField(default=False, null=True, blank=True)
     price = models.DecimalField(max_digits=6, decimal_places=2)
     rating = models.DecimalField(max_digits=6, decimal_places=2, null=True,
                                  blank=True)

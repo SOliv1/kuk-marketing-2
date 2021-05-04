1784fb1d824ee003dc3fbab7c0150dbda7162285
SOliv1 <s.oliver1@icloud.com>
2021-05-04 16:55:25 +0100

    make it better

---
+++ env.py
@@ -0,0 +1,10 @@
+import os
+
+os.environ["databaseUrl"] = "postgres://hqnorrmjiichoz:66175c4c28088402b3713a75e33a8a980d5e47d15c23891e8f8271ee491587fa@ec2-34-242-89-204.eu-west-1.compute.amazonaws.com:5432/dddi7qfhe50t8o"
+os.environ["AWS_SECRET_ACCESS_KEY"] = "RnPlA5mdamy3gEJvtzjFvHtRxMudLGCb/nlR8EIK"
+os.environ["AWS_ACCESS_KEY"] = "AKIAV7RSAYFHNFZEZC2V"
+os.environ["SECRET_KEy"] = "f-x$f_y#qk91366ff08-0qlip5*s$zr*@jsof+_xsamy##3&mk"
+os.environ[""] = ""
+os.environ[""] = ""
+os.environ[""] = ""
+os.environ[""] = ""
--- video/models.py
+++ video/models.py
@@ -1,12 +1,15 @@
 from django.db import models
+from embed_video.fields import EmbedVideoField
 
 
 # Create your models here.
+
+class Item(models.Model):
+    video = EmbedVideoField()  # same like models.URLField()
+
+
 class Video(models.Model):
     caption=models.CharField(max_length=100)
     video=models.FileField(upload_to="video/%y")
     def __str__(self):
         return self.caption
-
-
-

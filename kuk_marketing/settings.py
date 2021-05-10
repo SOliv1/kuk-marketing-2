a4976f250b30e684edcc79dca5ca3b11ff357798
Sara Oliver <s.oliver1@icloud.com>
2021-05-10 13:34:18 +0000

    video

--- kuk_marketing/settings.py
+++ kuk_marketing/settings.py
@@ -51,6 +51,7 @@ INSTALLED_APPS = [
     'home',
     'products',
     'bag',
+    'checkout',
     'profiles',
     'services',
     'embed_video',
--- kuk_marketing/urls.py
+++ kuk_marketing/urls.py
@@ -26,4 +26,5 @@ urlpatterns = [
     path('checkout/', include('checkout.urls')),
     path('profile/', include('profiles.urls')),
     path('services/', include('services.urls')),
+    path('video/', include('video.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

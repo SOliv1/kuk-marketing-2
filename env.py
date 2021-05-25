cd4219c2680749b6e499f69b462954d527d2f3c4
Sara Oliver <s.oliver1@icloud.com>
2021-05-25 22:54:17 +0000

    commit

---
+++ env.py
@@ -0,0 +1,12 @@
+import os
+
+os.environ["DATABASE_URL"] = 'postgres://obzqduskvunxlz:29405ae7f274ea50d0d293ed1b7ccbe99ee155b96acadd60c37bf8967a79ae22@ec2-34-255-134-200.eu-west-1.compute.amazonaws.com:5432/d3qqdltk2g2u2n'
+os.environ["AWS_ACCESS_KEY"] = 'AKIAV7RSAYFHEGXMCXEZ'
+os.environ["AWS_SECRET_ACCESS_KEY"] = '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'
+os.environ["USE_AWS"] = 'True'
+os.environ["SECRET_KEY"] = '%ll84jyn^q&=yiz@qn2q@myjja218q#ci-nn=)a5g(s8io&ke_'
+os.environ["HOST"] = 'localhost'
+os.environ["PORT"] = '8000'
+os.environ["STRIPE_PUBLIC_KEY"] = 'pk_test_51GtAGjDvyPDUJbikVWyYOumfWpQlZdzXnhDKRPPeNvSX0RTApmHnUmOvnsgpHwaqoUUp5ekqlKl8xxHlcFyKKvVT00WWP5gmou'
+os.environ["STRIPE_SECRET_KEY"] = 'sk_test_51GtAGjDvyPDUJbikpwurphl2orPFN3yLUIWg7GhBxnGFoyPtIhS1RjlSldLJjsItel7d2OWONC3Yj8uPudKqXpmS00hqKMIbtF'
+os.environ["STRIPE_WH_SECRET"] = 'whsec_d0VtPS475fux0wHkakneXKbAeXHInXn4'
